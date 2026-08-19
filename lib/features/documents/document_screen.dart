93533f54240e417bd1fa8271053e9b8c7850e639
Tamer Eldosoky <dosoky.server@gmail.com>
2026-08-19 22:23:16 +0300

    fix(dart): secure file_picker import and restore platform pickFiles call

--- lib/features/documents/document_screen.dart
+++ lib/features/documents/document_screen.dart
@@ -44,7 +44,7 @@ class _DocumentScreenState extends State<DocumentScreen> {
   }
 
   Future<void> _pickFile() async {
-    FilePickerResult? result = await FilePicker.instance.pickFiles(
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
       type: FileType.custom,
       allowedExtensions: ['txt'],
     );
